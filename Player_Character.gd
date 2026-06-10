b810f03d07a8dad9d7534518bb4257304357aa5d
kori31415 <korikobylak3141@gmail.com>
2026-06-10 13:47:44 -0500

    Added Sprint Toggle & Made Smoother Movement

--- Player_Character.gd
+++ Player_Character.gd
@@ -3,26 +3,31 @@ extends CharacterBody2D
 @onready var _animation_player = $Animations
 
 const tile_size = 16
-const move_speed = 0.5
+var move_speed = 0.5
 var moving = false
 var input_direction
 
 func _physics_process(delta: float) -> void:
 	if not moving:
 		input_direction = Vector2.ZERO
-		if Input.is_action_just_pressed("Left"):
+		if Input.is_action_pressed("Left"):
 			input_direction = Vector2.LEFT
 			_animation_player.play("Face_Left")
-		elif Input.is_action_just_pressed("Right"):
+		elif Input.is_action_pressed("Right"):
 			input_direction = Vector2.RIGHT
 			_animation_player.play("Face_Right")
-		elif Input.is_action_just_pressed("Up"):
+		elif Input.is_action_pressed("Up"):
 			input_direction = Vector2.UP
 			_animation_player.play("Face_Up")
-		elif Input.is_action_just_pressed("Down"):
+		elif Input.is_action_pressed("Down"):
 			input_direction = Vector2.DOWN
 			_animation_player.play("Face_Down")
 		move()
+	if Input.is_action_just_pressed("Cancel"):
+		if move_speed == 0.5:
+			move_speed = 0.25
+		else:
+			move_speed = 0.5
 
 func move():
 	if input_direction:
