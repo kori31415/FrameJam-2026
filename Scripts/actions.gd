d59abd7800f3dc2aa5aa54cf8a5dd29253ebec38
kori31415 <korikobylak3141@gmail.com>
2026-06-11 13:16:46 -0500

    Orginization & Actions as Resources
    Added orginizational folders an action class as a resource for ease of making new actions.

---
+++ Scripts/actions.gd
@@ -0,0 +1,10 @@
+class_name Actions
+extends Resource
+
+@export var name : String
+@export var description :  String
+
+@export var damage : int = 0
+@export var defense:  int = 0
+@export var buff: int = 0
+@export var echo_incremental: int = 0
