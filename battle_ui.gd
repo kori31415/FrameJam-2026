f06ae8a20648fe0107c0ca02de7061c7db2e3524
kori31415 <korikobylak3141@gmail.com>
2026-06-11 12:06:39 -0500

    Basic Battle UI
    Keyboard inputs will be edited and improved in the future.

---
+++ battle_ui.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$HBoxContainer/Attack.grab_focus.call_deferred()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_attack_pressed() -> void:
+	print("Attack, add attack to queue")
+
+
+func _on_defend_pressed() -> void:
+	print("Defend, add defense to queue")
+
+
+func _on_skills_pressed() -> void:
+	print("Open Skill Menu")
