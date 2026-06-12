f1e85378d02b1b79bea1a0c42df94c6f216e5b70
kori31415 <korikobylak3141@gmail.com>
2026-06-12 15:58:05 -0500

    Player Movment Improved
    Added Screen Transitions, Rooms + Rooms Folder, and wall collision

--- Scripts/Player_Character.gd
+++ Scripts/Player_Character.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 @onready var _animation_player = $Animations
+@onready var ray_cast = $RayCast2D
 
 const tile_size = 16
+var ray_length = tile_size
 var move_speed = 0.5
 var moving = false
 var input_direction
@@ -14,18 +16,23 @@ func _physics_process(delta: float) -> void:
 		input_direction = Vector2.ZERO
 		if Input.is_action_pressed("Left"):
 			input_direction = Vector2.LEFT
+			ray_cast.target_position = input_direction * ray_length
 			_animation_player.play("Face_Left")
 		elif Input.is_action_pressed("Right"):
 			input_direction = Vector2.RIGHT
+			ray_cast.target_position = input_direction * ray_length
 			_animation_player.play("Face_Right")
 		elif Input.is_action_pressed("Up"):
 			input_direction = Vector2.UP
+			ray_cast.target_position = input_direction * ray_length
 			_animation_player.play("Face_Up")
 		elif Input.is_action_pressed("Down"):
 			input_direction = Vector2.DOWN
+			ray_cast.target_position = input_direction * ray_length
 			_animation_player.play("Face_Down")
-		if can_move:
-			move()
+	ray_cast.force_raycast_update()
+	if can_move && !ray_cast.is_colliding():
+		move()
 	if Input.is_action_just_pressed("Cancel"):
 		if move_speed == 0.5:
 			move_speed = 0.25
@@ -33,7 +40,7 @@ func _physics_process(delta: float) -> void:
 			move_speed = 0.5
 
 func move():
-	if input_direction:
+	if input_direction != Vector2.ZERO:
 		if moving == false:
 			moving = true
 			var tween = create_tween()
