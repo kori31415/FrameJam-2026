323b6b4182488f6209fe7357c5148d64a637ee55
kori31415 <korikobylak3141@gmail.com>
2026-06-21 14:57:53 -0500

    Moved Keys

--- Scripts/doorway.gd
+++ Scripts/doorway.gd
@@ -1,14 +1,15 @@
-extends Area2D
+extends StaticBody2D
 
 @onready var room = get_parent()
 @onready var animation = $AnimatedSprite2D
 @onready var light_blocker = $LightOccluder2D
 @export var locked = false
+@export var connected_key = ""
 #@export var linked_key = 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	unlock_door()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
@@ -16,12 +17,12 @@ func _on_body_entered(_body: Node2D) -> void:
 	animation.play()
 	light_blocker.occluder_light_mask = 0
 
-func _on_body_exited(_body: Node2D) -> void:
-	animation.play_backwards()
-	light_blocker.occluder_light_mask = 1
-
 func lock_doors():
 	if !locked:
 		collision_layer = false
 		
-#func unlock_door():
+func unlock_door():
+	if connected_key in globals.collected_keys:
+		collision_layer = false
+		animation.play()
+		light_blocker.occluder_light_mask = 0
